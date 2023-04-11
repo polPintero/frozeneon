2bca586fbc03d820a630d648ec70206e67c6b5e1
Dmitriy <johndoe@example.com>
2023-04-11 10:54:19 +0300

    basic created header

---
+++ src/components/HeaderComp.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-app-bar app class="header">
+    <v-text-field solo label="Search" prepend-inner-icon="mdi-file-document-box-search-outline" dense clearable
+      hide-details></v-text-field>
+  </v-app-bar>
+</template>
+
+<script>
+export default {
+  name: "HeaderComp",
+};
+</script>
+<style lang="scss">
+.header {
+  .v-toolbar__content {
+    justify-content: center;
+  }
+
+  .v-input {
+    flex-grow: unset;
+    width: 350px;
+  }
+}
+</style>
