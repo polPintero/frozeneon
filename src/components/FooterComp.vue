9e69479f01a6e4e059d9225a8f808b87e878ac67
Dmitriy <johndoe@example.com>
2023-04-11 10:52:44 +0300

    created footer

---
+++ src/components/FooterComp.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-footer class="footer">
+    <v-row justify="center" no-gutters>
+      <a href="https://ru.stackoverflow.com/users/271553/%d0%94%d0%bc%d1%8b%d1%82%d1%80%d1%8b%d0%ba" target="_blank"><img
+          src="https://ru.stackoverflow.com/users/flair/271553.png?theme=clean" width="208" height="58"/>
+      </a>
+      <a class="social-links__item" target="_blank" href="https://github.com/polPintero">
+        <v-icon size="48px">mdi-github-box</v-icon>
+      </a>
+    </v-row>
+  </v-footer>
+</template>
+
+<script>
+export default {
+  name: 'FooterComp',
+};
+</script>
+
+<style lang="scss">
+.footer {
+  .row {
+    align-items: center;
+
+    a {
+      margin-right: 24px;
+    }
+  }
+}
+</style>
