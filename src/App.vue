da74439ca3d3efd029bed6442493b88c314c56f8
Dmitriy <johndoe@example.com>
2023-04-11 10:53:01 +0300

    basic created app

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+   <v-app>
+     <v-layout>
+       <FooterComp></FooterComp>
+     </v-layout>
+   </v-app>
+</template>
+
+
+
+<script setup>
+import { ref } from 'vue'
+import api from './api'
+import FooterComp from './components/FooterComp.vue'
+
+let searchResult = ref()
+getData()
+
+
+async function getData () {
+  const response =  await api.getSearch('vue')
+  searchResult.value = response.results
+}
+
+
+</script>
