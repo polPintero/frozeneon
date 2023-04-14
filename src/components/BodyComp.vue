c97e17811ab2ea9e775d99eba94c77c3408cedf5
Dmitriy <johndoe@example.com>
2023-04-14 13:56:40 +0300

    added nodata block

--- src/components/BodyComp.vue
+++ src/components/BodyComp.vue
@@ -1,11 +1,16 @@
 <template>
   <v-main class="v-body">
     <div class="v-body__content">
-      <v-hover v-slot="{ isHovering, props }" v-for="item in packagesList" :key="item">
-        <v-card :elevation="isHovering ? 24 : 6" class="mx-auto pa-6" v-bind="props" :title="item.package?.name"
-          :subtitle="item.package?.author?.name" :text="item.package?.version" @click="setActiveItem(item.package)">
-        </v-card>
-      </v-hover>
+      <template v-if="packagesList.length === 0">
+      <div  class="v-body__content--no-data">No Data</div>
+    </template>
+      <template v-else>
+        <v-hover v-slot="{ isHovering, props }" v-for="item in packagesList" :key="item">
+          <v-card :elevation="isHovering ? 24 : 6" class="mx-auto pa-6" v-bind="props" :title="item.package?.name"
+            :subtitle="item.package?.author?.name" :text="item.package?.version" @click="setActiveItem(item.package)">
+          </v-card>
+        </v-hover>
+      </template>
     </div>
 
     <v-pagination v-if="pageCount" class="mt-10" v-model="currPage" :length="pageCount" :total-visible="6"></v-pagination>
@@ -16,7 +21,7 @@
           {{ activeItem.name }}
         </v-card-title>
         <v-card-text>
-          {{ activeItem.name }}
+          {{ activeItem.description }}
           <SocialLinks :links="activeItem.links"></SocialLinks> 
         </v-card-text>
 
@@ -35,7 +40,7 @@ import { watch, reactive, ref } from 'vue'
 import { storeToRefs } from 'pinia'
 import { globalStore } from '@/store/index'
 
-import SocialLinks from './SocialLinks.vue';
+import SocialLinks from '@/components/SocialLinks.vue';
 
 const store = globalStore()
 const { getSearch } = store
@@ -63,6 +68,11 @@ function setActiveItem (item) {
     .v-card {
       cursor: pointer;
       width: 100%;
+    }
+
+    &--no-data {
+      text-align: center;
+      font-size: 24px;
     }
   }
 }
